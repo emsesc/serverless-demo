84da44cffea99c3924ef87d23951e98f0ba1e1ef
emsesc <emseschen@gmail.com>
2022-04-09 19:23:46 -0400

    w3s2

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,13 +1,52 @@
+var multipart = require("parse-multipart")
+const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const { BlobServiceClient } = require("@azure/storage-blob");
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    var boundary = multipart.getBoundary(req.headers['content-type']);
+    var body = req.body;
+    var parsedBody = multipart.Parse(body, boundary);
+    context.log(parsedBody);
+    
+    var filetype = parsedBody[0].type;
+    if (filetype == "image/png") {
+        ext = "png";
+    } else if (filetype == "image/jpeg") {
+        ext = "jpeg";
+    } else {
+        username = "invalidimage"
+        ext = "";
+    }
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    var responseMessage = await uploadFile(parsedBody, ext);
     context.res = {
-        // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
+    console.log(responseMessage)
+}
+
+async function uploadFile(parsedBody, ext){
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+    // Create a unique name for the container
+    const containerName = "images";
+    
+    console.log('\nCreating container...');
+    console.log('\t', containerName);
+    
+    // Get a reference to a container
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    
+    // Create the container
+    const blobName = 'test' + "." + ext;
+
+    // Get a block blob client
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    
+    console.log('\nUploading to Azure storage as blob:\n\t', blobName);
+    
+    // Upload data to the blob
+    const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
+    console.log("Blob was uploaded successfully. requestId: ", uploadBlobResponse.requestId);
+    return "File Saved";    
 }
